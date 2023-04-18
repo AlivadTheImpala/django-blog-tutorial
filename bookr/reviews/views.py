f8e0df9fe6a3c1007ceef8959fc0386f3167e5a2
AlivadTheImpala <brandon.davila@seattlecolleges.edu>
2023-04-17 18:31:06 -0700

    added search_result.html template

--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    name = "Brandon"
+    return render(request, "base.html", {"name": name})
+
+
+def book_search(request):
+    # below we make a request using method GET, so we can look at the URL when
+    # we then use lil get, to view the search parameter of the URL. ie the users search terms.
+    # if the user doesn't enter anything in a form using GET then search_text will return an empty string
+
+    search_text = request.GET.get("search", "")
+    return render(request, "search_result.html", {"search_text": search_text})
