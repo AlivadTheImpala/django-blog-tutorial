16eca9c4a9007a5b2d00fa520539ab68adf99880
AlivadTheImpala <brandon.davila@seattlecolleges.edu>
2023-04-21 14:19:40 -0700

    added models for review application

--- bookr/reviews/models.py
+++ bookr/reviews/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Publisher(models.Model):
+    """A Company that Publishes books."""
+    name = models.CharField(max_length=50,
+                            help_text="The name of the Publisher.")
+    website = models.URLField(help_text="The publishers website.")
+    email = models.EmailField(help_text="The publishers email address.")
+
+
+class Book(models.Model):
+    """A published book."""
+    title = models.CharField(max_length=70,
+                             help_text="The title of the Book.")
+    publication_date = models.DateField(verbose_name="Date the book was published")
+    isbn = models.CharField(max_length=20, verbose_name="ISBN number of the book.")
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+
+
+class Contributor(models.Model):
+    """A contributor to a book, e.g author, editor, co-author"""
+    first_names = models.CharField(max_length=50, help_text="The contributors first name or names.")
+    last_names = models.CharField(max_length=50, help_text="The contributors last name or names")
+    email = models.EmailField(help_text="The contributors contact email")
