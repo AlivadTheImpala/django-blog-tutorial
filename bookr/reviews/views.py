41a891798542dba356851f01b83afbef864262ea
AlivadTheImpala <brandon.davila@seattlecolleges.edu>
2023-04-30 17:32:23 -0700

    adds book details view

--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -1,14 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Book
+from .utils import average_rating
 
 
 def index(request):
-    return render(request, "base.html")
+    return render(request, "reviews/base.html")
 
 
 def book_search(request):
-    # below we make a request using method GET, so we can look at the URL when
-    # we then use lil get, to view the search parameter of the URL. ie the users search terms.
-    # if the user doesn't enter anything in a form using GET then search_text will return an empty string
-
     search_text = request.GET.get("search", "")
-    return render(request, "search_result.html", {"search_text": search_text})
+    return render(request, "reviews/search-results.html", {"search_text": search_text})
+
+
+def book_list(request):
+    books = Book.objects.all()
+    books_with_reviews = []
+    for book in books:
+        reviews = book.review_set.all()
+        if reviews:
+            book_rating = average_rating([review.rating for review in reviews])
+            number_of_reviews = len(reviews)
+        else:
+            book_rating = None
+            number_of_reviews = 0
+        books_with_reviews.append({"book": book, "book_rating": book_rating, "number_of_reviews": number_of_reviews})
+
+    context = {
+        "book_list": books_with_reviews
+    }
+    return render(request, "reviews/book_list.html", context)
+
+
+def book_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    reviews = book.review_set.all()
+    if reviews:
+        book_rating = average_rating([review.rating for review in reviews])
+        context = {
+            "book": book,
+            "book_rating": book_rating,
+            "reviews": reviews
+        }
+    else:
+        context = {
+            "book": book,
+            "book_rating": None,
+            "reviews": None
+        }
+    return render(request, "reviews/book_detail.html", context)
