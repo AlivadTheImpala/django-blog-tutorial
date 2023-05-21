603fbc1f223ffcefd6974a25ddfcccfc94884eb7
AlivadTheImpala <brandon.davila@seattlecolleges.edu>
2023-05-21 15:22:55 -0700

    added business_site project

--- bookr/reviews/admin.py
+++ bookr/reviews/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from reviews.models import (Publisher, Contributor, Book,
-                            BookContributor, Review)
+# from django.contrib.admin import AdminSite
+from reviews.models import (Publisher, Contributor, Book, BookContributor, Review)
+
+
+# class BookrAdminSite(AdminSite):
+#     title_header = 'Bookr Admin'
+#     site_header = 'Bookr administration'
+#     index_title = 'Bookr site admin'
+#     admin_site = BookrAdminSite(name='bookr')
 
 
 class BookAdmin(admin.ModelAdmin):
