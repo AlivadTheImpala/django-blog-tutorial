ee7175ef0fb341653f0e83b8a27afb9c5f60f48a
AlivadTheImpala <brandon.davila@seattlecolleges.edu>
2023-05-28 21:09:05 -0700

    added "add review" functionality

--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -1,8 +1,9 @@
 from django.contrib import messages
 from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 
-from .forms import PublisherForm, SearchForm
-from .models import Book, Contributor, Publisher
+from .forms import PublisherForm, SearchForm, ReviewForm
+from .models import Book, Contributor, Publisher, Review
 from .utils import average_rating
 
 
@@ -96,4 +97,39 @@ def publisher_edit(request, pk=None):
         form = PublisherForm(instance=publisher)
 
     return render(request, "reviews/instance-form.html", {"form": form, "instance": publisher,
-                                                         "model_type": "Publisher"})
+                                                          "model_type": "Publisher"})
+
+
+def review_edit(request, book_pk, review_pk=None):
+    book = get_object_or_404(Book, pk=book_pk)
+
+    if review_pk is not None:
+        review = get_object_or_404(Review, book_id=book_pk, pk=review_pk)
+    else:
+        review = None
+
+    if request.method == "POST":
+        form = ReviewForm(request.POST, instance=review)
+
+        if form.is_valid():
+            updated_review = form.save(False)
+            updated_review.book = book
+
+            if review is None:
+                messages.success(request, "Review for \"{}\" created.".format(book))
+            else:
+                updated_review.date_edited = timezone.now()
+                messages.success(request, "Review for \"{}\" updated.".format(book))
+
+            updated_review.save()
+            return redirect("book_detail", book.pk)
+    else:
+        form = ReviewForm(instance=review)
+
+    return render(request, "reviews/instance-form.html",
+                  {"form": form,
+                   "instance": review,
+                   "model_type": "Review",
+                   "related_instance": book,
+                   "related_model_type": "Book"
+                   })
